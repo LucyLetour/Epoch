9d5ef017b1a40a3137c75c94b3e5c78673d5e569
Ender_L <bluestackthingie@gmail.com>
2018-09-10 13:09:37 -0400

    Changed the space tiles to a .zktx
    #1 The open space tilemap now sources its tileset images form a .zktx file which is better than different .png's

--- core/src/com/epochgames/epoch/util/Assets.java
+++ core/src/com/epochgames/epoch/util/Assets.java
@@ -5,12 +5,11 @@ import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGeneratorLoader;
+import com.badlogic.gdx.maps.tiled.AtlasTmxMapLoader;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 /**
@@ -25,7 +24,7 @@ public class Assets {
     // Initialize the asset manager by constructing it.
     static {
         MANAGER = new AssetManager();
-        MANAGER.setLoader(TiledMap.class, new TmxMapLoader(new InternalFileHandleResolver()));
+        MANAGER.setLoader(TiledMap.class, new AtlasTmxMapLoader(new InternalFileHandleResolver()));
         MANAGER.setLoader(FreeTypeFontGenerator.class, new FreeTypeFontGeneratorLoader(new InternalFileHandleResolver()));
     }
 
